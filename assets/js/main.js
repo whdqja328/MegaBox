b1fed8da797e274422d6f00d2817dc85bf601d9e
whdqja328 <whdqja328@naver.com>
2021-06-20 15:50:47 +0900

    JS_slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,6 +87,61 @@ rankBtnContainer.addEventListener('click',(e)=>{
 
 })
 
+// Slider
+document.addEventListener("DOMContentLoaded", function () {
+  // 변수 지정
+  const slideWrap = document.querySelector(".slide_wrap"),
+    slideContainer = document.querySelector(".slide_container"),
+    slides = document.querySelectorAll(".slide"),
+    pagerBtn = document.querySelectorAll(".pager span");
+
+  let slideCount = slides.length,
+    currentIndex = 0,
+    timer = null;
+
+  // 슬라이드 가로로 배열하기
+  for (let i = 0; i < slideCount; i++) {
+    slides[i].style.left = i * 100 + "%";
+  }
+
+  // 슬라이드 이동함수
+  function goToSlide(idx) {
+    slideContainer.classList.add("animate");
+    slideContainer.style.left = -100 * idx + "%";
+    currentIndex = idx;
+
+    for (let y = 0; y < pagerBtn.length; y++) {
+      pagerBtn[y].classList.remove("on");
+    }
+    pagerBtn[idx].classList.add("on");
+
+    for (let s = 0; s < slideCount; s++) {
+      slides[s].classList.remove("fade_in");
+    }
+    slides[idx].classList.add("fade_in");
+  }
+
+  goToSlide(0);
+
+  //자동 슬라이드 함수
+  function startAutoSlide() {
+    timer = setInterval(function () {
+      let nextIdx = (currentIndex + 1) % slideCount;
+
+      goToSlide(nextIdx);
+    }, 4500);
+  }
+
+  startAutoSlide();
+
+  // 슬라이드 정지 함수
+  function stopAutoSlide() {
+    clearInterval(timer);
+  }
+
+  //pager로 슬라이드 이동하기
+  
+});
 
 
 
